36b9f1263ea4f8949f533262eb095d014a69df5c
Agrim-ani <agrimani.125@gmail.com>
2022-10-27 14:58:57 +0530

    added roles authentication

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const { ROLE, users } = require('./data')
+const {authRole, authUser } = require('./basicAuth')
+const projectRouter = require('./routes/projects')
+
+app.use(express.json())
+app.use(setUser)
+app.use('/projects', projectRouter)
+
+app.get('/', (req, res) => {
+  res.send('Home Page')
+})
+
+app.get('/dashboard',authUser,  (req, res) => {
+  res.send('Dashboard Page')
+})
+
+app.get('/admin',authUser,authRole(ROLE.ADMIN), (req, res) => {
+  res.send('Admin Page')
+})
+
+//if that user exists then continue otherwise make sure they are logged in 
+function setUser(req, res, next) {
+  const userId = req.body.userId
+  if (userId) {
+    req.user = users.find(user => user.id === userId)
+  }
+  next()
+}
+
+app.listen(3000)
